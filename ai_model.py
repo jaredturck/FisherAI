7413e3e3a3102073e3ac967eef76c0a924379f95
Jared Turck <jaredturck8@gmail.com>
2025-10-27 16:35:16 +0000

    Added forward pass

--- ai_model.py
+++ ai_model.py
@@ -13,7 +13,7 @@ class ChessDataset(Dataset):
         return len(self.training_data)
     
     def __getitem__(self, idx):
-        return self.training_data[idx]
+        return self.training_data[idx], self.training_data[idx + 1]
     
     def read_data(self):
         samples = []
@@ -37,4 +37,29 @@ class FisherAI(Module):
         self.piece_type_embedding = nn.Embedding(6, 2)
         self.color_embedding = nn.Embedding(2, 2)
         self.position_embedding = nn.Embedding(self.d_model, 2)
-        
+        self.em_dropout = nn.Dropout(self.dropout)
+
+        self.encoder_layer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(
+                d_model=self.d_model,
+                nhead=self.nheads,
+                dim_feedforward=self.dim_feedforward,
+                dropout=self.dropout,
+                batch_first=True
+            ),
+            num_layers=self.no_transformer_layers
+        )
+
+    def forward(self, x):
+        piece_type = x[:, :, 0].long()
+        color = x[:, :, 1].long()
+
+        piece_type_emb = self.piece_type_embedding(piece_type)
+        color_emb = self.color_embedding(color)
+
+        x = piece_type_emb + color_emb + position_emb
+        x = self.em_dropout(x)
+
+        x = self.encoder_layer(x)
+
+        return x
